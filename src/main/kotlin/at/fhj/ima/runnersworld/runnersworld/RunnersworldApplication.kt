e442d9127fd1b79b0f3aea43d327b99ceab8925b
Raphael Hackensoellner <raphael.hackensoellner@edu.fh-joanneum.at>
2020-05-24 11:26:16 +0200

    Base Setup - Initial Creation

---
+++ src/main/kotlin/at/fhj/ima/runnersworld/runnersworld/RunnersworldApplication.kt
@@ -0,0 +1,11 @@
+package at.fhj.ima.runnersworld.runnersworld
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RunnersworldApplication
+
+fun main(args: Array<String>) {
+	runApplication<RunnersworldApplication>(*args)
+}
---
+++ src/test/kotlin/at/fhj/ima/runnersworld/runnersworld/RunnersworldApplicationTests.kt
@@ -0,0 +1,13 @@
+package at.fhj.ima.runnersworld.runnersworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RunnersworldApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
